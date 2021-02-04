766352b579fad21e9abaacdf8bf7db23e6dafa01
kevm124 <kmurciab@purdue.edu>
2021-02-04 14:59:45 -0500

    Added initial models

---
+++ AdWebScraper/Models/Advert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdWebScraper.Models
+{
+    public class Advert
+    {
+        public int _id { get; set; }
+        public string Url { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Description { get; set; }
+        public Car Car { get; set; }
+    }
+}
---
+++ AdWebScraper/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdWebScraper.Models
+{
+    public class Car
+    {
+        public int _id { get; set; }
+        public string MakeModel { get; set; }
+        public uint Year { get; set; }
+        public uint Price { get; set; }
+        public uint Miles { get; set; }
+        public string Condition { get; set; }
+        public string Color { get; set; }
+
+        public int AdvertId { get; set; }
+        public Advert Advert { get; set; }
+    }
+}
