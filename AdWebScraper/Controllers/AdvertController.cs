59dcd649f94e960bc1c718016084b6c687244ccb
kevm124 <kmurciab@purdue.edu>
2021-02-04 15:06:22 -0500

    Added basic advert controller class

---
+++ AdWebScraper/Controllers/AdvertController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdWebScraper.Services;
+using AdWebScraper.Models;
+
+namespace AdWebScraper.Controllers
+{
+    [Route("/api/[controller]")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertService _advertService;
+
+        public AdvertController(IAdvertService advertService)
+        {
+            _advertService = advertService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Advert>> GetAllAsync()
+        {
+            var adverts = await _advertService.ListAsync();
+            return adverts;
+        }
+    }
+}
