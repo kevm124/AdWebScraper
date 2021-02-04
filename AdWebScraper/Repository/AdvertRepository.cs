51859920fd9dddbc806755430f7f66178a845c2a
kevm124 <kmurciab@purdue.edu>
2021-02-04 17:00:26 -0500

    Added advert repository to handle data access logic

---
+++ AdWebScraper/Repository/AdvertRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AdWebScraper.Models;
+using AdWebScraper.Contexts;
+
+namespace AdWebScraper.Repository
+{
+    public class AdvertRepository : BaseRepository, IAdvertRepository
+    {
+        public AdvertRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Advert>> ListAsync()
+        {
+            return await _context.Adverts.ToListAsync();
+        }
+    }
+}
---
+++ AdWebScraper/Repository/IAdvertRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+
+namespace AdWebScraper.Repository
+{
+    public interface IAdvertRepository
+    {
+        Task<IEnumerable<Advert>> ListAsync();
+    }
+}
