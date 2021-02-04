a02fb8dee91c96c029e47da33a62c187a57a4a89
kevm124 <kmurciab@purdue.edu>
2021-02-04 15:25:39 -0500

    Added basic advert service

---
+++ AdWebScraper/Services/AdvertService.cs
@@ -0,0 +1,24 @@
+﻿using AdWebScraper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Repository;
+
+namespace AdWebScraper.Services
+{
+    public class AdvertService : IAdvertService
+    {
+        private readonly IAdvertRepository _advertRepository;
+
+        public AdvertService(IAdvertRepository advertRepository)
+        {
+            _advertRepository = advertRepository;
+        }
+
+        public async Task<IEnumerable<Advert>> ListAsync()
+        {
+            return await _advertRepository.ListAsync();
+        }
+    }
+}
---
+++ AdWebScraper/Services/IAdvertService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+
+namespace AdWebScraper.Services
+{
+    public interface IAdvertService
+    {
+        Task<IEnumerable<Advert>> ListAsync();
+    }
+}
