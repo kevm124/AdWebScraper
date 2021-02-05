c40a76c63297385174058266533004b1b3a726d1
kevm124 <kmurciab@purdue.edu>
2021-02-05 13:52:23 -0500

    Added response classes for advert and car

---
+++ AdWebScraper/Services/Communication/AdvertResponse.cs
@@ -0,0 +1,20 @@
+﻿using AdWebScraper.Models;
+
+namespace AdWebScraper.Services.Communication
+{
+    public class AdvertResponse : BaseResponse
+    {
+        public Advert Advert { get; private set; }
+
+        private AdvertResponse(bool success, string message, Advert advert) : base(success, message)
+        {
+            Advert = advert;
+        }
+
+        //Creates success response
+        public AdvertResponse(Advert advert) : this(true, string.Empty, advert) { }
+
+        //Creates an error response
+        public AdvertResponse(string message) : this(false, message, null) { }
+    }
+}
---
+++ AdWebScraper/Services/Communication/CarResponse.cs
@@ -0,0 +1,20 @@
+﻿using AdWebScraper.Models;
+
+namespace AdWebScraper.Services.Communication
+{
+    public class CarResponse : BaseResponse
+    {
+        public Car Car { get; private set; }
+
+        private CarResponse(bool success, string message, Car car) : base(success, message)
+        {
+            Car = car;
+        }
+
+        //Creates success response
+        public CarResponse(Car car) : this(true, string.Empty, car) { }
+
+        //Creates an error response
+        public CarResponse(string message) : this(false, message, null) { }
+    }
+}
