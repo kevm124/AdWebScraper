b48850427ce5ad3eac751b91a66eb2194b95a8c4
kevm124 <kmurciab@purdue.edu>
2021-02-05 13:40:31 -0500

    Added a resource used when saving adverts

---
+++ AdWebScraper/Resources/SaveAdvertResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdWebScraper.Resources
+{
+    public class SaveAdvertResource
+    {
+        [Required]
+        [Url]
+        public string Url { get; set; }
+        public DateTime DatePosted { get; set; }
+        public String Description { get; set; }
+    }
+}
