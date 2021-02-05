80ac87605268e2ec58dcea26c8e4889ad8ae7333
kevm124 <kmurciab@purdue.edu>
2021-02-05 13:19:59 -0500

    Added resource for adverts and changed get endpoint to return resource

--- AdWebScraper/Controllers/AdvertController.cs
+++ AdWebScraper/Controllers/AdvertController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
 using AdWebScraper.Services;
 using AdWebScraper.Models;
+using AdWebScraper.Resources;
 
 namespace AdWebScraper.Controllers
 {
@@ -12,17 +14,21 @@ namespace AdWebScraper.Controllers
     public class AdvertController : ControllerBase
     {
         private readonly IAdvertService _advertService;
+        private readonly IMapper _mapper;
 
-        public AdvertController(IAdvertService advertService)
+        public AdvertController(IAdvertService advertService, IMapper mapper)
         {
             _advertService = advertService;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Advert>> GetAllAsync()
+        public async Task<IEnumerable<AdvertResource>> GetAllAsync()
         {
             var adverts = await _advertService.ListAsync();
-            return adverts;
+            var resources = _mapper.Map<IEnumerable<Advert>, IEnumerable<AdvertResource>>(adverts);
+
+            return resources;
         }
     }
 }
---
+++ AdWebScraper/Resources/AdvertResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+
+namespace AdWebScraper.Resources
+{
+    public class AdvertResource
+    {
+        public int _id { get; set; }
+        public string Url { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Description { get; set; }
+        public Car Car { get; set; }
+    }
+}
