50bd9cde3b5664bf1bc83a555664dddd8b2d256d
kevm124 <kmurciab@purdue.edu>
2021-02-07 15:34:13 -0500

    Added repository to make changes to cars databases

---
+++ AdWebScraper/Repository/CarRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AdWebScraper.Models;
+using AdWebScraper.Contexts;
+
+namespace AdWebScraper.Repository
+{
+    public class CarRepository : BaseRepository, ICarRepository
+    {
+        public CarRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Car>> ListAsync()
+        {
+            return await _context.Cars.ToListAsync();
+        }
+
+        public async Task AddAsync(Car car)
+        {
+            await _context.Cars.AddAsync(car);
+        }
+
+       
+        public async Task<Car> FindByIdAsync(int id)
+        {
+            return await _context.Cars.FindAsync(id);
+        }
+
+        
+        public void Update(Car car)
+        {
+            _context.Cars.Update(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _context.Cars.Remove(car);
+        }
+    }
+}
---
+++ AdWebScraper/Repository/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+
+namespace AdWebScraper.Repository
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> ListAsync();
+        Task AddAsync(Car car);
+        Task<Car> FindByIdAsync(int id);
+        void Update(Car car);
+        void Delete(Car car);
+    }
+}
