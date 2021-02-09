720f0d048af770d089cfb0a9d0ad705ab17a8740
kevm124 <kmurciab@purdue.edu>
2021-02-09 14:26:45 -0500

    Added a car service class to complete save, update, delete, and list transactions on car database

---
+++ AdWebScraper/Services/CarService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+using AdWebScraper.Repository;
+using AdWebScraper.Services.Communication;
+
+namespace AdWebScraper.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CarService(ICarRepository carRepository, IUnitOfWork unitOfWork)
+        {
+            _carRepository = carRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Car>> ListAsync()
+        {
+            return await _carRepository.ListAsync();
+        }       
+       
+        public async Task<CarResponse> SaveAsync(Car car)
+        {
+            try
+            {
+                await _carRepository.AddAsync(car);
+                await _unitOfWork.CompleteAsync();
+
+                return new CarResponse(car);
+            }
+            catch(Exception e)
+            {
+                return new CarResponse($"An error occured when saving the car: {e.Message}");
+            }
+        }
+
+        public async Task<CarResponse> UpdateAsync(int id, Car car)
+        {
+            var existingCar = await _carRepository.FindByIdAsync(id);
+            if(existingCar == null)
+            {
+                return new CarResponse($"Car not found");
+            }
+
+            existingCar.MakeModel = car.MakeModel;
+            if(car.Year != 0)
+                existingCar.Year = car.Year;
+            if(car.Price != 0)
+                existingCar.Price = car.Price;
+            if (car.Miles != 0)
+                existingCar.Miles = car.Miles;
+            if (car.Condition != null)
+                existingCar.Condition = car.Condition;
+            if (car.Color != null)
+                existingCar.Color = car.Color;
+            if (car.AdvertId != 0)
+                existingCar.AdvertId = car.AdvertId;
+
+            try
+            {
+                _carRepository.Update(existingCar);
+                await _unitOfWork.CompleteAsync();
+
+                return new CarResponse(existingCar);
+            }
+            catch (Exception e)
+            {
+                return new CarResponse($"An error occured when saving the car: {e.Message}");
+            }
+        }
+
+        public async Task<CarResponse> DeleteAsync(int id)
+        {
+            var existingCar = await _carRepository.FindByIdAsync(id);
+            if (existingCar == null)
+            {
+                return new CarResponse($"Car not found");
+            }
+
+            try
+            {
+                _carRepository.Delete(existingCar);
+                await _unitOfWork.CompleteAsync();
+
+                return new CarResponse(existingCar);
+            }
+            catch (Exception e)
+            {
+                return new CarResponse($"An error occured when saving the car: {e.Message}");
+            }
+        }
+    }
+}
---
+++ AdWebScraper/Services/ICarService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdWebScraper.Models;
+using AdWebScraper.Services.Communication;
+
+namespace AdWebScraper.Services
+{
+    public interface ICarService
+    {
+        Task<IEnumerable<Car>> ListAsync();
+        Task<CarResponse> SaveAsync(Car car);
+        Task<CarResponse> UpdateAsync(int id, Car car);
+        Task<CarResponse> DeleteAsync(int id);
+    }
+}
