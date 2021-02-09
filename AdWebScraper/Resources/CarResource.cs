1f4ea90b5a381d1d9b5a77579403ab4cadb02cb1
kevm124 <kmurciab@purdue.edu>
2021-02-09 14:25:28 -0500

    Added resource models for input and output of cars

---
+++ AdWebScraper/Resources/CarResource.cs
@@ -0,0 +1,14 @@
+﻿namespace AdWebScraper.Resources
+{
+    public class CarResource
+    {      
+        public int _id { get; set; }
+        public string MakeModel { get; set; }
+        public uint Year { get; set; }
+        public uint Price { get; set; }
+        public uint Miles { get; set; }
+        public string Condition { get; set; }
+        public string Color { get; set; }
+        public int AdvertId { get; set; }
+    }
+}
---
+++ AdWebScraper/Resources/SaveCarResource.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdWebScraper.Resources
+{
+    public class SaveCarResource
+    {
+        [Required]
+        public string MakeModel { get; set; }
+        public uint Year { get; set; }
+        public uint Price { get; set; }
+        public uint Miles { get; set; }
+        public string Condition { get; set; }
+        public string Color { get; set; }
+        public int AdvertId { get; set; }
+    }
+}
