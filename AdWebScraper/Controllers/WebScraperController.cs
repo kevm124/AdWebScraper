d7b84457fd333c67bd197ef1f178364c2ddff99c
kevm124 <kmurciab@purdue.edu>
2021-02-10 19:13:08 -0500

    Added a basic controller for web scrapping function

---
+++ AdWebScraper/Controllers/WebScraperController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AdWebScraper.Services;
+
+namespace AdWebScraper.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebScraperController : ControllerBase
+    {
+        private readonly IWebScraperService _webScraperService;
+
+        public WebScraperController(IWebScraperService webScraperService)
+        {
+            _webScraperService = webScraperService;
+        }
+
+        // Get: api/WebScraper
+        [HttpGet]
+        public string Get()
+        {
+            var url = "https://indianapolis.craigslist.org/ctd/d/whiteland-2012-ford-fusion-hybrid/7275090025.html";
+            _webScraperService.GetPageData(url);
+
+            return "Hello world";
+        }        
+    }
+}
