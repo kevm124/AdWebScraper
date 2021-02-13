bb90e95d03969bc45f0cdaaab28d0b7f6c11832b
kevm124 <kmurciab@purdue.edu>
2021-02-13 13:43:23 -0500

    Added check to make sure url is a link to craigslist car and truck post

--- AdWebScraper/Controllers/WebScraperController.cs
+++ AdWebScraper/Controllers/WebScraperController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 using AutoMapper;
 using AdWebScraper.Services;
 using AdWebScraper.Resources;
@@ -28,6 +29,11 @@ namespace AdWebScraper.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAsync([FromBody] string url)
         {
+            if(!Regex.IsMatch(url, @"https:\/\/\w+.craigslist.org\/ct[od]\/d\/.+\.html"))
+            {
+                return BadRequest("URL is not valid");
+            }
+
             var result = await _webScraperService.GetPageData(url);
 
             if (!result.Success)
